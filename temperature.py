928a953d02ddb3bcddfcc7f899a6f3141b740a8e
James Kelly <jk377y@gmail.com>
2023-06-02 23:05:53 -0500

    finished logic and styling

--- temperature.py
+++ temperature.py
@@ -1,11 +1,17 @@
-import tkinter as tk # import the customtkinter module; used to create custom widgets and windows for standalone apps
+import tkinter as tk
+import customtkinter as ctk # import the customtkinter module; used to create custom widgets and windows for standalone apps
 
+ctk.set_appearance_mode("dark")  # Modes: system (OS settings decide), light, dark
+ctk.set_default_color_theme("dark-blue") # set the default color theme for all widgets; 'dark-blue', 'blue', and 'green' are available
 
 #? main window
 # ==============================================================================================
-window = tk.Tk()  # create the main window
+window = ctk.CTk()  # create the main window
 window.title("Temperature Converter")  # set the title of the main window
-window.geometry("510x270")  # set the size of the main window
+window.geometry("490x260")  # set the size of the main window
+window.resizable(False, False)  # make the main window non-resizable
+window.configure(bg="#243a57")  # set the background color of the main window
+window.iconbitmap("images/icon/MERN16.ico")
 # ==============================================================================================
 
 
@@ -13,31 +19,49 @@ window.geometry("510x270")  # set the size of the main window
 #? Conversion functions
 # ==============================================================================================
 def FahrenheitToCelcius(): # function to convert Fahrenheit to Celsius
+    if entry.get() == "":
+        result_label.configure(text="Please enter a value")
+        return
     temperature = float(entry.get()) # gets value from entry
     result = round((temperature - 32) * 5/9, 2)
     result_label.configure(text=f"{temperature}°F = {result}°C") # sends value to result_label
 
 def FahrenheitToKelvin(): # function to convert Fahrenheit to Kelvin
+    if entry.get() == "":
+        result_label.configure(text="Please enter a value")
+        return
     temperature = float(entry.get()) # gets value from entry
     result = round((temperature - 32) * 5/9 + 273.15, 2)
     result_label.configure(text=f"{temperature}°F = {result}°K") # sends value to result_label
 
 def CelciusToFahrenheit(): # function to convert Celsius to Fahrenheit
+    if entry.get() == "":
+        result_label.configure(text="Please enter a value")
+        return
     temperature = float(entry.get()) # gets value from entry
     result = round(temperature * 9/5 + 32, 2)
     result_label.configure(text=f"{temperature}°C = {result}°F") # sends value to result_label
 
 def CelciusToKelvin(): # function to convert Celsius to Kelvin
+    if entry.get() == "":
+        result_label.configure(text="Please enter a value")
+        return
     temperature = float(entry.get()) # gets value from entry
     result = round(temperature + 273.15, 2)
     result_label.configure(text=f"{temperature}°C = {result}°K") # sends value to result_label
 
 def KelvinToFahrenheit(): # function to convert Kelvin to Fahrenheit
+    if entry.get() == "":
+        result_label.configure(text="Please enter a value")
+        return
     temperature = float(entry.get()) # gets value from entry
     result = round((temperature - 273.15) * 9/5 + 32, 2)
     result_label.configure(text=f"{temperature}°K = {result}°F") # sends value to result_label
 
 def KelvinToCelcius(): # function to convert Kelvin to Celsius
+    if entry.get() == "":
+        result_label.configure(text="Please enter a value")
+        return
     temperature = float(entry.get()) # gets value from entry
     result = round(temperature - 273.15, 2)
     result_label.configure(text=f"{temperature}°K = {result}°C") # sends value to result_label
@@ -48,32 +72,34 @@ def KelvinToCelcius(): # function to convert Kelvin to Celsius
 #? elements inside the main window (children); input box, buttons, labels, etc.
 # ==============================================================================================
 
-label = tk.Label(window, text="Enter temperature:", font=("Arial", 20))  # create a label widget; child of root
-label.grid(row=0, column=1, pady=20, padx=10)  # make the label visible; pack it into the main window (root)
+label = ctk.CTkLabel(window, text="Enter temperature:", font=("Arial", 20))  # create a label widget; child of root
+label.grid(row=0, column=1, pady=10)  # make the label visible; pack it into the main window (root)
 
-entry = tk.Entry(window,) # create an entry widget; child of root; this is the input box
-entry.grid(row=1, column=1)
+entry = ctk.CTkEntry(window,) # create an entry widget; child of root; this is the input box
+entry.grid(row=1, column=1, pady=20)
 
-ctof_btn = tk.Button(window, text="Celsius to Fahrenheit", command=CelciusToFahrenheit)
-ctof_btn.grid(row=2, column=0)
+BUTTON = ""
 
-ctok_btn = tk.Button(window, text="Celsius to Kelvin", command=CelciusToKelvin)
-ctok_btn.grid(row=3, column=0)
+ctof_btn = ctk.CTkButton(window, text="Celsius to Fahrenheit", command=CelciusToFahrenheit)
+ctof_btn.grid(row=2, column=0, pady=5, padx=10)
 
-ftoc_btn = tk.Button(window, text="Fahrenheit to Celsius", command=FahrenheitToCelcius)
-ftoc_btn.grid(row=2, column=1)
+ctok_btn = ctk.CTkButton(window, text="Celsius to Kelvin", command=CelciusToKelvin)
+ctok_btn.grid(row=3, column=0, pady=5, padx=10)
 
-ftok_btn = tk.Button(window, text="Fahrenheit to Kelvin", command=FahrenheitToKelvin)
-ftok_btn.grid(row=3, column=1)
+ftoc_btn = ctk.CTkButton(window, text="Fahrenheit to Celsius", command=FahrenheitToCelcius)
+ftoc_btn.grid(row=2, column=1, pady=5)
 
-ktoc_btn = tk.Button(window, text="Kelvin to Celsius", command=KelvinToCelcius)
-ktoc_btn.grid(row=2, column=2)
+ftok_btn = ctk.CTkButton(window, text="Fahrenheit to Kelvin", command=FahrenheitToKelvin)
+ftok_btn.grid(row=3, column=1, pady=5)
 
-ktof_btn = tk.Button(window, text="Kelvin to Fahrenheit", command=KelvinToFahrenheit)
-ktof_btn.grid(row=3, column=2)
+ktoc_btn = ctk.CTkButton(window, text="Kelvin to Celsius", command=KelvinToCelcius)
+ktoc_btn.grid(row=2, column=2, pady=5, padx=10)
 
-result_label = tk.Label(window, text="", font=("Arial", 18))
-result_label.grid(row=4, column=0)
+ktof_btn = ctk.CTkButton(window, text="Kelvin to Fahrenheit", command=KelvinToFahrenheit)
+ktof_btn.grid(row=3, column=2, pady=5, padx=10)
+
+result_label = ctk.CTkLabel(window, text="", font=("Arial", 18), text_color="yellow")
+result_label.grid(row=4, column=0, pady=20, columnspan=3)
 # ==============================================================================================
 
 
